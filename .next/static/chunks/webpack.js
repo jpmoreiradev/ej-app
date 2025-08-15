4b4feadc46f67599ddaaee8563b2e6ad3b9f7b60
jpmoreiradev <jp02120123@gmail.com>
2025-08-15 18:06:38 -0300

    add: git ignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6dbeac4e06c6a29e"; }
+/******/ 		__webpack_require__.h = function() { return "2871257277eed225"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
